10f93aba74bd592857f300980bc419d1f03c8d69
luisencastin <luisencastin@gmail.com>
2023-04-14 14:59:42 -0700

    Added cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,24 +2,37 @@ import React from 'react'
 
 import './Card.css'
 
-function Card() {
+function Card({
+    characterImage,
+    characterName,
+    characterComics,
+    characterStories,
+    comics
+}) {
     return (
         <section className="card">
             <div className="cardInner">
                 <div className="cardFront">
                     <div className="imageContainer">
-
+                        <img src={characterImage} alt="character hero" className="cardImage"/>
                     </div>
                     <div className="cardMenuContainer">
-                        <p className="characterName">Nombre</p>
+                        <p className="characterName"><b>Nombre:</b> {characterName}</p>
                         <div className="cardSubcontainer">
-                            <p className="characterName">Categoria</p>
-                            <p className="characterName">Superpoder</p>
+                            <p className="characterComics"><b>Comics:</b> {characterComics}</p>
+                            <p className="characterStories"><b>Historias:</b> {characterStories}</p>
                         </div>
                     </div>                
                 </div>
                 <div className="cardBack">
-                    <p>Hola mundo</p>
+                    <h5>Comics en que ha aparecido:</h5>
+                    <ul>
+                        {
+                        comics.map(item => 
+                        <li className="comicNameItem">{item.name}</li>)
+                        }
+                    </ul>
+
                 </div>                
             </div>
 
--- src/components/CardsDisplayer/CardsDisplayer.jsx
+++ src/components/CardsDisplayer/CardsDisplayer.jsx
@@ -1,30 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Card from './../Card/Card';
-import './CardsDisplayer.css'
+import './CardsDisplayer.css';
+import md5 from 'md5';
 
 function CardsDisplayer() {
     const [data, setData] = useState([]);
+    const publicKey = process.env.REACT_APP_PUBLIC_API_KEY;
+    const privateKey = process.env.REACT_APP_PRIVATE_API_KEY;
+    const baseURL = process.env.REACT_APP_URL;
+    const ts = Date.now().toString();
+    const hash = md5(ts + privateKey + publicKey);
 
-    console.log(process.env.REACT_APP_URL)
-    // useEffect(() => {
-    //     fetchData();
-    //   }, []);
+    const fetchData = useCallback(async () => {
+        try {
+          const url = `${baseURL}characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          const responseJson = await response.json();
+          setData(responseJson.data.results);
+        } catch (err) {
+          console.log(err);
+        }
+      }, [baseURL, ts, publicKey, hash, setData]);
+
+    useEffect(() => {
+        fetchData();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+
+    console.log(data)
+
+    const mappedCards = data.map((item) =>{
+        console.log(item.thumbnail.path.split('/').slice(-1))
+        if (item.thumbnail.path.split('/').slice(-1).toString() !== 'image_not_available') {
+            console.log('holaaaa')
+            return (
+                <Card
+                characterImage={`${item.thumbnail.path}/portrait_uncanny.${item.thumbnail.extension}`}
+                characterName={item.name}
+                characterComics={item.comics.available}
+                characterStories={item.stories.available}
+                comics={item.comics.items}
+                />   
+            );
+          } else {
+            return null; 
+          }
+    })
+
+    // <Card
+    // characterImage={`${item.thumbnail.path}/portrait_uncanny.${item.thumbnail.extension}`}
+    // characterName={item.name}
+    // characterComics={item.comics.available}
+    // characterStories={item.stories.available}
+    // comics={item.comics.items}
+    // />   
 
     return (
         <section className="cardsDisplayer">
             <div className="mainCardsContainer">
-                <div className="cardsContainer">  
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
+                <div className="cardsContainer">
+                    { mappedCards }  
                 </div>
             </div>
             <div className="projectDescription">
